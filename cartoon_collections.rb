98d89de139dba83d3267afd8f696669210222f91
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-05 00:54:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(ingredients)
-  # the array below is here to help
+  ingredients.find { |e| cheese_types.include?(e) }
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
