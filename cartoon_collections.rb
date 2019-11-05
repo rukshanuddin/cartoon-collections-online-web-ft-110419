cc20ae8e39dfd6f9fd93db09fd49d6580a4081a2
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-05 00:49:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
-  calls.each{|call| puts "#{call.capitalize}!"}
+  calls.map{|call| puts "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
